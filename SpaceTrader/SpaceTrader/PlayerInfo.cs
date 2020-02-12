c188dd5bc6970e84b4cf323fc8abda9a57c4bb61
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-12 11:24:35 -0500

    Added PlayerSetup and PlayerRace methods

--- SpaceTrader/SpaceTrader/PlayerInfo.cs
+++ SpaceTrader/SpaceTrader/PlayerInfo.cs
@@ -8,7 +8,20 @@ namespace SpaceTrader
     {
         public PlayerInfo()
         {
+            PlayerSetup();
+        }
 
+        private void PlayerSetup()
+        {
+            Console.Write("What is your name?: ");
+            string endUserName = Console.ReadLine();
+            Console.WriteLine($"Welcome to SpaceTrade {endUserName}!");
+            Console.Clear();
+        }
+
+        private void PlayerRace()
+        {
+            //
         }
     }
 }
