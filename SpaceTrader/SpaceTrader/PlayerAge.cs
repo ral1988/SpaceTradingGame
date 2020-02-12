167da6b397976425247189bcf7382cb3719104d4
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-12 16:01:57 -0500

    Updating

--- SpaceTrader/SpaceTrader/PlayerAge.cs
+++ SpaceTrader/SpaceTrader/PlayerAge.cs
@@ -6,5 +6,6 @@ namespace SpaceTrader
 {
     class PlayerAge
     {
+        
     }
 }
--- SpaceTrader/SpaceTrader/Program.cs
+++ SpaceTrader/SpaceTrader/Program.cs
@@ -14,10 +14,8 @@ namespace Consolefullscreen
         private static IntPtr ThisConsole = GetConsoleWindow();
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        private const int HIDE = 0;
         private const int MAXIMIZE = 3;
-        private const int MINIMIZE = 6;
-        private const int RESTORE = 9;
+        
         static void Main(string[] args)
         {
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
