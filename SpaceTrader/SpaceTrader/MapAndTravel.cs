b9f10bc6387c0ab80dcfba6a7fdcce3c0e6522ab
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-12 10:23:59 -0500

    Map stuff

--- SpaceTrader/SpaceTrader/MapAndTravel.cs
+++ SpaceTrader/SpaceTrader/MapAndTravel.cs
@@ -6,5 +6,21 @@ namespace SpaceTrader
 {
     class MapAndTravel
     {
+        static void DisplayMap()
+        {
+            Console.WriteLine(u+2440, u+2551, u+2554, u+2557, u+255D, u+2591, u+2742, u+2739, u+2726, u+2738, u+2736, u+2737);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
     }
 }
