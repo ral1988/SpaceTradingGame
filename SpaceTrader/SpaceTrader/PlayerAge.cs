8a9d1b0205ad122176e0e3e8bd38754f9d21aba1
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-12 11:19:56 -0500

    added EndOfCareer method in PlayerAge class

--- SpaceTrader/SpaceTrader/PlayerAge.cs
+++ SpaceTrader/SpaceTrader/PlayerAge.cs
@@ -6,9 +6,17 @@ namespace SpaceTrader
 {
     class PlayerAge
     {
+        int startAge = 18;
+
         PlayerAge()
         {
 
         }
+
+        private void EndOfCareer()
+        {
+            //logic for subtracting travel, action, and other event times from the age of 65. 
+            //Graphical display of how much time you have left
+        } 
     }
 }
