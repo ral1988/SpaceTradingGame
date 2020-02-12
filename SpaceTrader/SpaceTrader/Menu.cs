0b7165d52fb884539cce1342a7088749fbebf1cb
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-12 14:16:06 -0500

    Fullscreen added.

--- SpaceTrader/SpaceTrader/Menu.cs
+++ SpaceTrader/SpaceTrader/Menu.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SpaceTrader
+﻿namespace SpaceTrader
 {
     class Menu
     {
+        
     }
 }
--- SpaceTrader/SpaceTrader/Program.cs
+++ SpaceTrader/SpaceTrader/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
-namespace SpaceTrader
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+namespace Consolefullscreen
 {
     class Program
     {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
         static void Main(string[] args)
         {
-            new MapAndTravel().DisplayMap();
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            new SpaceTrader.MapAndTravel().DisplayMap();
+
         }
     }
 }
