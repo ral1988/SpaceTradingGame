373d5d8c396a8ab6fed72437023658506f141ae1
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-13 10:25:12 -0500

    Moved Tuple to mapcoord

---
+++ SpaceTrader/SpaceTrader/MapCoord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTrader
+{
+    class MapCoord
+    {
+        MapCoord()
+        {
+
+        }
+
+        public void planetLocations()
+        {
+            Tuple<string, double, double>[] plantLocation =
+            {new Tuple<string, double, double>("Sol", 0.0, 0.0),
+             new Tuple<string, double, double>("ProximaCentauriB", 2.646, -3.0),
+             new Tuple<string, double, double>("BarnardsStarB", 4.0, 4.472),
+             new Tuple<string, double, double>("Ross128B", 0.0, -11.0),
+             new Tuple<string, double, double>("LuytensStarB", -10.392, -6.0),
+             new Tuple<string, double, double>("Wolf1061C", 9.798, 10.0),
+             new Tuple<string, double, double>("Gliese832C", 15.0, -5.56776),
+             new Tuple<string, double, double>("HD8832G", -13.601, 16.0),
+            };
+        }
+    }
+}
--- SpaceTrader/SpaceTrader/SupplyAndDemand.cs
+++ SpaceTrader/SpaceTrader/SupplyAndDemand.cs
@@ -16,12 +16,12 @@ namespace SpaceTrader
         {
 
             var basePrice = 100;                    //baseprice is universal across the map based on the item type
-            var localDemand = PlanetInfo.;        //planet determines the local demand
-            var demandFactor = ;                  //ratio: planets overall demand for item / planet total
-            var unitsInDemand = PlanetInfo.;      //a ratio of how much the planet you are at wants this item
-            var localSupply = PlanetInfo.;        //What is in supply on the local planet
-            var supplyFactor = ;                  //How much of the item is currently available - how the universe started with
-            var unitsSupplied = a;                //how many the end user is requesting
+            var localDemand = PlanetInfo.;          //planet determines the local demand
+            var demandFactor = ;                    //ratio: planets overall demand for item / planet total
+            var unitsInDemand = PlanetInfo.;        //a ratio of how much the planet you are at wants this item
+            var localSupply = PlanetInfo.;          //What is in supply on the local planet
+            var supplyFactor = ;                    //How much of the item is currently available - how the universe started with
+            var unitsSupplied = a;                  //how many the end user is requesting
 
             var price = basePrice * (localDemand + demandFactor * unitsInDemand) / (localSupply + supplyFactor * unitsSupplied);
 
