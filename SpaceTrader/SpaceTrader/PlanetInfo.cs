a0caf05262dd44e57a669572484547fed8bc223e
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-13 09:58:00 -0500

    added shell of planet information

--- SpaceTrader/SpaceTrader/PlanetInfo.cs
+++ SpaceTrader/SpaceTrader/PlanetInfo.cs
@@ -11,6 +11,8 @@ namespace SpaceTrader
         public PlanetInfo(string planetName)
         {
             this.planetName = planetName;
+
+
             Tuple<string, double, double>[] plantLocation =
             {new Tuple<string, double, double>("Sol", 0.0, 0.0),
              new Tuple<string, double, double>("ProximaCentauriB", 2.646, -3.0),
@@ -33,44 +35,112 @@ namespace SpaceTrader
             
             var temp = 58.63m;
             string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
             double mass = 1.0;
-            double grav = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
             string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
 
         }
         private void ProximaCentauriB()
         {
-
+            var temp = -274.0m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
 
         private void BarnardsStarB()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
 
         private void Ross128B()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
 
         private void LuytensStarB()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
 
         private void Wolf1061C()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
         
         private void Gliese832C()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
 
         private void HD8832G()
         {
-
+            var temp = 58.63m;
+            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            var light = "Perfect";
+            string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
+            double mass = 1.0;
+            string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
+            double gravity = 1.0;
+            string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
+            string atmosphere = "Earth";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
         }
     }
 }
