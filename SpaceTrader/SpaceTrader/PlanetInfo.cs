0f0155d5f1f168885d7af9d789c715116c24d508
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-13 09:47:40 -0500

    created global tuple for planet locations

--- SpaceTrader/SpaceTrader/PlanetInfo.cs
+++ SpaceTrader/SpaceTrader/PlanetInfo.cs
@@ -11,6 +11,16 @@ namespace SpaceTrader
         public PlanetInfo(string planetName)
         {
             this.planetName = planetName;
+            Tuple<string, double, double>[] plantLocation =
+            {new Tuple<string, double, double>("Sol", 0.0, 0.0),
+             new Tuple<string, double, double>("ProximaCentauriB", 2.646, -3.0),
+             new Tuple<string, double, double>("BarnardsStarB", 4.0, 4.472),
+             new Tuple<string, double, double>("Ross128B", 0.0, -11.0),
+             new Tuple<string, double, double>("LuytensStarB", -10.392, -6.0),
+             new Tuple<string, double, double>("Wolf1061C", 9.798, 10.0),
+             new Tuple<string, double, double>("Gliese832C", 15.0, -5.56776),
+             new Tuple<string, double, double>("HD8832G", -13.601, 16.0),
+            };
         }
 
         public string CurrentPlanet()
@@ -20,8 +30,12 @@ namespace SpaceTrader
 
         private void Sol()
         {
+            
             var temp = 58.63m;
             string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
+            double mass = 1.0;
+            double grav = 1.0;
+            string atmosphere = "Earth";
 
         }
         private void ProximaCentauriB()
