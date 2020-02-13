7889179cc4b414ce8220c2373e6e07488c2f4436
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-13 11:45:01 -0500

    planet class

--- SpaceTrader/SpaceTrader/App.cs
+++ SpaceTrader/SpaceTrader/App.cs
@@ -10,15 +10,95 @@ namespace SpaceTrader
         {
             var currentPlanet = 0;
 
-            List<string> StarChart = new List<string>();
-            StarChart.Add("SOL");
+            List<Planet> StarChart = new List<Planet>();
+            /*StarChart.Add("SOL");
             StarChart.Add("PC-B");
             StarChart.Add("BS-B");
             StarChart.Add("ROSS-128");
             StarChart.Add("LS-B");
             StarChart.Add("Wolf-1061");
             StarChart.Add("Gliese-832");
-            StarChart.Add("HR8832-G");
+            StarChart.Add("HR8832-G");*/
+            Planet SOL = new Planet
+            {
+                Name = "SOL",
+                Coord = new Location
+                {
+                    x = 0,
+                    y = 0
+                }
+
+            };
+            Planet PCB = new Planet
+            {
+                Name = "PC-B",
+                Coord = new Location
+                {
+                    x = 2.646,
+                    y = -3
+                }
+
+            };
+            Planet BSB = new Planet
+            {
+                Name = "BS-B",
+                Coord = new Location
+                {
+                    x = 4,
+                    y = 4.472
+                }
+
+            };
+            Planet ROSS128 = new Planet
+            {
+                Name = "ROSS-128",
+                Coord = new Location
+                {
+                    x = 0,
+                    y = -11
+                }
+
+            };
+            Planet LSB = new Planet
+            {
+                Name = "LS-B",
+                Coord = new Location
+                {
+                    x = -10.392,
+                    y = -6
+                }
+
+            };
+            Planet WOLF1061 = new Planet
+            {
+                Name = "WOLF-1061",
+                Coord = new Location
+                {
+                    x = 9.798,
+                    y = 10
+                }
+
+            };
+            Planet GLIESE832 = new Planet
+            {
+                Name = "GLIESE-832",
+                Coord = new Location
+                {
+                    x = 15,
+                    y = -5.56776
+                }
+
+            };
+            Planet HR8832G = new Planet
+            {
+                Name = "HR8832-G",
+                Coord = new Location
+                {
+                    x = -13.601,
+                    y = 16
+                }
+
+            };
 
             new SpaceTrader.Menu();
 
---
+++ SpaceTrader/SpaceTrader/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTrader
+{
+    class Planet
+    {
+       
+        public Planet()
+        {
+
+        }
+
+        public string Name;
+
+        public Location Coord;
+        
+        
+    }
+}
