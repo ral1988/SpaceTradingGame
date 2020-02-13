3aa3a9e5be83cafcf44b72100f511ab07042c514
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-13 11:25:10 -0500

    added location class

---
+++ SpaceTrader/SpaceTrader/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SpaceTrader
+{
+    public class Location : IEquatable<Location>
+    {
+        public double x { get; set; }
+
+        public double y { get; set; }
+
+        public bool Equals([AllowNull] Location other)
+        {
+            return this.x == other.x && this.y == other.y;
+        }
+    }
+}
