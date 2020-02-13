f3337ec70455faa82f324bf28ca4b91848f25528
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-13 10:04:08 -0500

    Planet information added up to Ross128B

--- SpaceTrader/SpaceTrader/PlanetInfo.cs
+++ SpaceTrader/SpaceTrader/PlanetInfo.cs
@@ -47,21 +47,21 @@ namespace SpaceTrader
         }
         private void ProximaCentauriB()
         {
-            var temp = -274.0m;
+            var temp = -38.0m;
             string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
-            var light = "Perfect";
+            var light = "no brighter than twilight";
             string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
-            double mass = 1.0;
+            double mass = 1.27;
             string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
-            double gravity = 1.0;
+            double gravity = 1.1;
             string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
-            string atmosphere = "Earth";
-            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
+            string atmosphere = "thick Hydrogen Clouds on surface, thin atmosphere otherwise";
+            string atmosphereOfPlanet = String.Format($"The atmosphere has {atmosphere}");
         }
 
         private void BarnardsStarB()
         {
-            var temp = 58.63m;
+            var temp = -274.0m;
             string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
             var light = "Perfect";
             string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
@@ -75,16 +75,17 @@ namespace SpaceTrader
 
         private void Ross128B()
         {
-            var temp = 58.63m;
-            string tempatureOutside = String.Format($"The tempature on the surface is {temp}°F");
-            var light = "Perfect";
+            var temp1 = 92.0m;
+            var temp2 = 62.0m;
+            string tempatureOutside = String.Format($"The tempature on the surface is \n{temp1}°F on the light side and {temp2}°F on the dark side");
+            var light = "38% brighter than Earth";
             string surfaceLight = String.Format($"The surface light on this planet is {light} times Earth");
-            double mass = 1.0;
+            double mass = 1.35;
             string massOfPlanet = String.Format($"The mass on this planet is {mass} times Earth");
-            double gravity = 1.0;
+            double gravity = 1.12;
             string gravityOfPlanet = String.Format($"The gravity on this planet is {gravity} times Earth");
-            string atmosphere = "Earth";
-            string atmosphereOfPlanet = String.Format($"The atmosphere is like {atmosphere}");
+            string atmosphere = "earth like";
+            string atmosphereOfPlanet = String.Format($"The atmosphere is {atmosphere}");
         }
 
         private void LuytensStarB()
