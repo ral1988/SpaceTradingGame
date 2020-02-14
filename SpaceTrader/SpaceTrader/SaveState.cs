740e5aa86145bbeb12a8b9cb23a9820e9b82b53c
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-14 12:30:03 -0500

    added notes on supply and demand

--- SpaceTrader/SpaceTrader/SaveState.cs
+++ SpaceTrader/SpaceTrader/SaveState.cs
@@ -7,4 +7,7 @@ namespace SpaceTrader
     class SaveState
     {
     }
+
+    //save current state of game
+    //maybe the logic will be here to load that state
 }
--- SpaceTrader/SpaceTrader/SupplyAndDemand.cs
+++ SpaceTrader/SpaceTrader/SupplyAndDemand.cs
@@ -17,7 +17,7 @@ namespace SpaceTrader
 
             /*var basePrice = 100;                    //baseprice is universal across the map based on the item type
             var localDemand = PlanetInfo;          //planet determines the local demand
-            var demandFactor = ;                    //ratio: planets overall demand for item / planet total
+            var demandFactor = ;                    //ratio: planets overall demand for item / planet total (7)
             var unitsInDemand = PlanetInfo.;        //a ratio of how much the planet you are at wants this item
             var localSupply = PlanetInfo.;          //What is in supply on the local planet
             var supplyFactor = ;                    //How much of the item is currently available - how the universe started with
