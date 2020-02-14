949c92f2a540458e1af23665f5584636b061b8f7
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-14 11:34:40 -0500

    made warpcompute return double

--- SpaceTrader/SpaceTrader/Warp.cs
+++ SpaceTrader/SpaceTrader/Warp.cs
@@ -8,8 +8,7 @@ namespace SpaceTrader
 
     class Warp
 	{
-		public double f = 0.0;
-		public double p = 10.0 / 3.0;
+		
 
 		/* //distance TravelDistance in MapAndTravelDistance
 		var factor;
@@ -290,27 +289,32 @@ namespace SpaceTrader
 			}
 		}
 		*/
-		public void WarpCompute(double distance)   
+		public static double WarpCompute(double distance)   
 		{
-			double warpInput = 5.0;
+		double f = 0.0;
+		double p = 10.0 / 3.0;
+		double warpInput = 5.0;
 
-			if (warpInput > 9.0)
-			{
-				var f = -0.5 * Math.Log(10 - warpInput) / Math.Log(10);
+		//if (warpInput > 9.0)
+		//{
+		//	var f = -0.5 * Math.Log(10 - warpInput) / Math.Log(10);
+		//}
+
+		// compute some results
+		var result = Math.Pow(warpInput, p + f);
+		
+		if (result > 0)
+		   {
+				var velocity =  distance / result;
+				return velocity;
 			}
 
-			// compute some results
-			var result = Math.Pow(warpInput, p + f);
-			
-			if (result > 0)
-		    {
-				var velocity =  distance / result;
-				}
-
-			else
-				{
-					var timeOfTravel = 365.25 * distance / result;
-				}
+		else
+			{
+				var timeOfTravel = 365.25 * distance / result;
+				return timeOfTravel;
+			}
+		
 		    
 		}
 
