334f8a4caccc48e9b33dc1e138833d5c3583d712
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-17 12:14:04 -0500

    Added flavor text class.

---
+++ SpaceTrader/SpaceTrader/FlavorText.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTrader
+{
+    class FlavorText
+    {
+
+        void SolMain()
+        {
+            Console.WriteLine($"\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u2551 As your ship flys through the atmosphere down to the surface, you look out your window and admire the \u2551");
+            Console.WriteLine("\u2551 beauty of the mountains, rivers and oceans of Earth. No matter what planet you visit, none really     \u2551");
+            Console.WriteLine("\u2551 compare to it's simple beauty.                                                                        \u2551");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u2551 You hear the tower hailing you as you get closer to the Columbus Eperopolitan Interstellar Spaceport. \u2551");
+            Console.WriteLine("\u2551 'Roma-Seven-Aught-Three-Niner, report to Landing Section 33, Pad 104.'        'Roger, Tower. 3 mikes.'\u2551");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u2551 Y                                                                                                      \u2551");
+            Console.WriteLine("\u2551 This is your story.                                                                                   \u2551");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u2551 Press C to Continue.                                                                                  \u2551");
+            Console.WriteLine("\u2551                                                                                                       \u2551");
+            Console.WriteLine("\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+
+
+
+        }
+
+        void PCBMain()
+        {
+
+
+        }
+
+        void BSBMain()
+        {
+
+        }
+
+        void Ross128Main()
+        {
+            
+        }
+
+        void LSBMain()
+        {
+
+        }
+
+        void Wolf1061Main()
+        {
+
+        }
+
+        void Gliese832Main()
+        {
+
+        }
+
+        void HR8832Main()
+        {
+
+        }
+
+
+
+
+    }
+}
