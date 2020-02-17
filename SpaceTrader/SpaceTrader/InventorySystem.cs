613055ac578570630abc7290e3545d6bd5998758
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-17 11:19:27 -0500

    added some code to try and figure out inventory.

---
+++ SpaceTrader/SpaceTrader/InventorySystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceTrader
+{
+    class InventorySystem
+    {
+        /*private const int MAXIMUM_SLOTS_IN_INVENTORY = 20;
+
+        public readonly List<InventoryRecord> InventoryRecords = new List<InventoryRecord>();
+ 
+        public void AddItem(ObtainableItem item, int quantityToAdd)
+        {
+            while (quantityToAdd > 0)
+            {
+                // If an object of this item type already exists in the inventory, and has room to stack more items,
+                // then add as many as we can to that stack.
+                if (InventoryRecords.Exists(x => (x.InventoryItem == item) && (x.Quantity < item.MaximumStackableQuantity)))
+                {
+                    // Get the item we're going to add quantity to
+                    InventoryRecord inventoryRecord =
+                    InventoryRecords.First(x => (x.InventoryItem == item) && (x.Quantity < item.MaximumStackableQuantity));
+
+                    // Calculate how many more can be added to this stack
+                    int maximumQuantityYouCanAddToThisStack = (item.MaximumStackableQuantity - inventoryRecord.Quantity);
+
+                    // Add to the stack (either the full quanity, or the amount that would make it reach the stack maximum)
+                    int quantityToAddToStack = Math.Min(quantityToAdd, maximumQuantityYouCanAddToThisStack);
+
+                    inventoryRecord.AddToQuantity(quantityToAddToStack);
+
+                    // Decrease the quantityToAdd by the amount we added to the stack.
+                    // If we added the total quantityToAdd to the stack, then this value will be 0, and we'll exit the 'while' loop.
+                    quantityToAdd -= quantityToAddToStack;
+                }
+                else
+                {
+                    // We don't already have an existing inventoryRecord for this ObtainableItem object,
+                    // so, add one to the list, if there is room.
+                    if (InventoryRecords.Count < MAXIMUM_SLOTS_IN_INVENTORY)
+                    {
+                        // Don't set the quantity value here.
+                        // The 'while' loop will take us back to the code above, which will add to the quantity.
+                        InventoryRecords.Add(new InventoryRecord(item, 0));
+                    }
+                    else
+                    {
+                        // Throw an exception, or somehow let the user know they are out of inventory space.
+                        // This exception here is just a quick example. Do something better in your code.
+                        throw new Exception("There is no more space in the inventory");
+                    }
+                }
+
+            }
+
+        }*/
+    }
+}
