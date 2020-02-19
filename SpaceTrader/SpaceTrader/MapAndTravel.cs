23e49303341a32a578aa6557bdbe8b421d401559
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-19 10:45:21 -0500

    some age changes again

--- SpaceTrader/SpaceTrader/MapAndTravel.cs
+++ SpaceTrader/SpaceTrader/MapAndTravel.cs
@@ -13,7 +13,6 @@ namespace SpaceTrader
         }
         public void DisplayMap(List<Planet> StarChart, ref int currentPlanet, ref PlayerAge callAge, OreType Titanium, OreType Gold, OreType Platinum, OreType Vibranium, InventorySystem inventory, TradingItems Food, TradingItems Water, TradingItems Textiles, TradingItems Electronics, TradingItems SpareParts, DangerousItems CapturedHydrogen, DangerousItems PoloniumFuel, IllegalItems Spice, IllegalItems Weapons, ref double Money, ref int FuelTank)
         {
-            callAge.TimeCalc(18);
 
             Console.ResetColor();
             // Display for the Map
