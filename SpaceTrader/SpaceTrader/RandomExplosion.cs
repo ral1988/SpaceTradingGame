8100c5d3a0e324dca069fd5e9d275b78ba51db97
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-20 08:04:39 -0500

    stuff

---
+++ SpaceTrader/SpaceTrader/RandomExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceTrader
+{
+    class RandomExplosion
+    {
+        public RandomExplosion()
+        {
+
+        }
+
+    }
+}
