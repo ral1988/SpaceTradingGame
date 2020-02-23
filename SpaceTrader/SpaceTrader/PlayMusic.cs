06b3b00e0a89e8e76f30755dd58a9e884f4d166e
Ryan Lee <ryan.a.lee1988@outlook.com>
2020-02-22 19:07:36 -0500

    adding PlayMusic class

---
+++ SpaceTrader/SpaceTrader/PlayMusic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+using System.Windows;
+
+
+namespace SpaceTrader
+{
+    class PlayMusic
+    {
+        public PlayMusic()
+        {
+            Console.Beep();
+        }
+
+        public void MidiPLayer()
+        {
+            
+        }
+    }
+}
