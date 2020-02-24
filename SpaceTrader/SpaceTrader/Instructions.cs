ad0f94a724577af524019c600520221fd4080c9f
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-24 09:44:21 -0500

    sync

--- SpaceTrader/SpaceTrader/Instructions.cs
+++ SpaceTrader/SpaceTrader/Instructions.cs
@@ -4,27 +4,30 @@ using System.Text;
 
 namespace SpaceTrader
 {
-    class Intro
+    class Instructions
     {
-        public Intro()
+        public Instructions()
         {
             Console.WriteLine("\u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 You have been in the SYNDICATE for as long as you can remember.                                       \u2551");
+            Console.WriteLine("\u2551 This is a game that is all about trading. Your goal is to buy and sell items and make a profit.       \u2551");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 You have seen enough violence and death for a hundred lifetimes.                                      \u2551");
+            Console.WriteLine("\u2551 There are several ways to make money. Mining, Buying/Selling Commodities, Buying/Selling Fuel Items,  \u2551");
+            Console.WriteLine("\u2551 and Buying/Selling Illicit Items.                                                                     \u2551");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 You have had enough. It is time for you to buy your freedom.                                          \u2551");
+            Console.WriteLine("\u2551 You can buy/sell mined Ore, Commodities, and some Fuel items at the Trading Post.                     \u2551");
+            Console.WriteLine("\u2551 You can you can buy Polonium Fuel Rods and Compressed Hydrogen and buy/sell Spaceship Fuel at the     \u2551");
+            Console.WriteLine("\u2551 Fuel Depot. Be Careful! Polonium Fuel Rods and Compressed Hydrogen can explode if you're not careful! \u2551");
+            Console.WriteLine("\u2551 Also, your ship has a max inventory of 20 tons of cargo so be wise with your purchases!               \u2551");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 Glurut, Grand Master of Death, says if you pay him $\u03B2 10,000,000 Space Bucks he will set you          \u2551");
-            Console.WriteLine("\u2551 free.                                                                                                 \u2551");
+            Console.WriteLine("\u2551 You can buy/sell Illicit Items at the Cantina. Be careful, if your ship gets inspected, you will be   \u2551");
+            Console.WriteLine("\u2551 arrested, half your money and all of your cargo will be seized! Also, you can only store 2 tons of    \u2551");
+            Console.WriteLine("\u2551 items in your smugglers hold.                                                                         \u2551");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 After your father died, he left you with his spaceship, the NOVIS, $\u03B2 1,000 and a small mining rig.   \u2551");
+            Console.WriteLine("\u2551 Each Star System has a planet you can land on. Each has different prices on goods so the name of the  \u2551");
+            Console.WriteLine("\u2551 game is buy low, sell high!                                                                           \u2551");
             Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 As you strap yourself into your seat, you feel the weight of your choice to leave the SYNDICATE on    \u2551");
-            Console.WriteLine("\u2551 your chest. To fail is to die.                                                                        \u2551");
-            Console.WriteLine("\u2551                                                                                                       \u2551");
-            Console.WriteLine("\u2551 You set your eyes on your gauges and focus on the task ahead...                                       \u2551");
+            Console.WriteLine("\u2551 Good luck and have fun!                                                                               \u2551");
             Console.WriteLine("\u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563");
             Console.WriteLine("\u2551                                                                                                       \u2551");
             Console.WriteLine("\u2551 Press C to Continue.                                                                                  \u2551");
@@ -41,9 +44,6 @@ namespace SpaceTrader
             }
 
             Console.Clear();
-
-           
-
         }
     }
 }
