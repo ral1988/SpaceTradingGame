a02fe59b53822c2cc5f294d99cdc682fb9d7da39
Thomas Loy <59874741+thomasloy1@users.noreply.github.com>
2020-02-27 10:07:16 -0500

    Added a thing to the victory screen

--- SpaceTrader/SpaceTrader/VictoryScreen.cs
+++ SpaceTrader/SpaceTrader/VictoryScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace SpaceTrader
 {
@@ -14,6 +15,8 @@ namespace SpaceTrader
                 Console.WriteLine();
                 Console.WriteLine("You have gotten $\u03B2 10,000,000 Space Bucks and Glurut, Grand Master of Death, is pleased and has granted you your freedom.");
                 Console.WriteLine("You can now begin to live your own life. So long traveler. May the Schwarz be with you!");
+
+                Thread.Sleep(20000);
                 Environment.Exit(0);
             }
         }
